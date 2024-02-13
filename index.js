1e6d5d8c83f18f937522ce3d6a571ef762ddac5a
Trismegiste <flo@trismegiste.net>
2024-02-13 09:51:50 +0100

    AlpineJS ok

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ import { Hono } from 'hono'
 import { serveStatic } from 'hono/bun';
 
 const app = new Hono()
-app.use('/style.css', serveStatic({path: './node_modules/@picocss/pico/css/pico.classless.pink.min.css'}))
-app.get('/', serveStatic({ path: './templates/index.html'}))
+
+// static
+app.use('/pico.css', serveStatic({path: './node_modules/@picocss/pico/css/pico.classless.yellow.min.css'}))
+app.use('/alpine.js', serveStatic({path: './node_modules/alpinejs/dist/module.esm.js'}))
+
+// controllers
+app.get('/', serveStatic({path: './templates/index.html'}))
 
 export default {
     port: 3000,
